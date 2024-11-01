05eb82db65cc4e2b2a226d793f61d94b598b64d7
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-11-01 22:17:20 +0700

    Fitur page done, kelasss

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,18 +10,18 @@ import './globals.css';
  */
 
 export const metadata = {
-	title: 'NuSantap',
-	description: 'NuSantap - GovAI Hackathon',
+  title: 'NuSantap',
+  description: 'NuSantap - GovAI Hackathon',
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body className="font-light antialiased">
-				<div className="flex h-screen w-screen items-center justify-center bg-gray-100">
-					<div className="relative flex h-full max-h-[888px] w-full max-w-[450px] flex-col items-center justify-between overflow-hidden bg-gray-500 px-4 py-8 shadow-2xl">{children}</div>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="font-light antialiased">
+        <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+          <div className="relative h-full max-h-[888px] w-full max-w-[450px] overflow-hidden bg-gray-500 shadow-2xl">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
 }
